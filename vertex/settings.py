d47c366f11ff4be78073e659c7957815e87af1bf
Dipec001 <dpecchukwu@gmail.com>
2024-09-28 09:06:48 +0100

    "Updated SECRET_KEY and DEBUG to use environment variables, and added allowed hosts"

--- vertex/settings.py
+++ vertex/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r(^3eqc-wx@15plm_^nd0mh656*+wow$ig7ll4+tq4lgb-31v2"
+SECRET_KEY = os.getenv('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default='False').lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','vertexx-85dc684c56f3.herokuapp.com']
 
 
 # Application definition
