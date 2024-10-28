d1f035257c0c411285b291f80a3df255f38cf07d
Dipec001 <dpecchukwu@gmail.com>
2024-10-28 20:11:07 +0100

    "Added duplicate removal for DailySteps model, added date field to DailySteps model, and updated migrations to include these changes."

--- myapp/migrations/0031_streak_date_alter_streak_timestamp_and_more.py
+++ myapp/migrations/0031_streak_date_alter_streak_timestamp_and_more.py
@@ -16,10 +16,20 @@ def remove_streak_duplicates(apps, schema_editor):
     for duplicate in duplicates:
         Streak.objects.filter(user=duplicate['user'], timeStamp__date=duplicate['timeStamp__date']).exclude(id=duplicate['max_id']).delete()
 
+def remove_dailysteps_duplicates(apps, schema_editor):
+    DailySteps = apps.get_model('myapp', 'DailySteps')
+    duplicates = (
+        DailySteps.objects.values('user', 'timestamp__date')
+        .annotate(max_id=models.Max('id'), count_id=models.Count('id'))
+        .filter(count_id__gt=1)
+    )
+    for duplicate in duplicates:
+        DailySteps.objects.filter(user=duplicate['user'], timestamp__date=duplicate['timestamp__date']).exclude(id=duplicate['max_id']).delete()
+
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('myapp', '0030_populate_date_field'),  # Adjust this to the correct last migration dependency
+        ('myapp', '0030_populate_date_field'),
     ]
 
     operations = [
@@ -28,10 +38,21 @@ class Migration(migrations.Migration):
             name='date',
             field=models.DateField(null=True),
         ),
+        migrations.AddField(
+            model_name='dailysteps',
+            name='date',
+            field=models.DateField(null=True),
+        ),
         migrations.RunPython(remove_streak_duplicates),
         migrations.RunPython(populate_streak_date_field),
+        migrations.RunPython(remove_dailysteps_duplicates),
         migrations.AlterField(
             model_name='streak',
+            name='date',
+            field=models.DateField(),
+        ),
+        migrations.AlterField(
+            model_name='dailysteps',
             name='date',
             field=models.DateField(),
         ),
