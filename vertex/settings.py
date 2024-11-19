6c849c872ea6644875531afd231ebb0dbb9c798d
Dipec001 <dpecchukwu@gmail.com>
2024-11-19 13:41:13 +0100

    "Updated CHANNEL_LAYERS configuration and added django_heroku settings"

--- vertex/settings.py
+++ vertex/settings.py
@@ -134,20 +134,10 @@ ASGI_APPLICATION = "vertex.asgi.application"
 
 
 # Channels
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            'CONNECTION_POOL_KWARGS': {
-                'ssl_cert_reqs': None  # Disables SSL verification
-            },
-        "CONFIG": {
-            "hosts": [('rediss://:p7db66ecbbc9fa16fe8e2ff70b1b8037dcb05ed6d7fbc24ecb2088a6de37bba89@ec2-107-23-186-192.compute-1.amazonaws.com:7070')],
-        },
-        }
-    },
-}
+import django_heroku 
+import ssl 
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379') 
+CHANNEL_LAYERS = { "default": { "BACKEND": "channels_redis.core.RedisChannelLayer", "CONFIG": { "hosts": [REDIS_URL], }, }, }
 # Check if we're in debug mode (local environment) or production (Heroku)
 # if os.getenv('DEBUG', 'False') == 'True':
 #     # Local development
@@ -397,3 +387,6 @@ CELERY_BEAT_SCHEDULE = {
 #         "continuous_profiling_auto_start": True,
 #     },
 # )
+
+# Activate Django-Heroku. 
+django_heroku.settings(locals())
