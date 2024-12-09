5839fa406219dabeaa594c4989d3c730bfb37d3b
Tsirimaholy <hei.tsirimaholy@gmail.com>
2024-12-09 16:57:24 +0300

    chore: remove api/ prefix

--- myapp/urls.py
+++ myapp/urls.py
@@ -56,6 +56,6 @@ urlpatterns = [
     path('send-notification/', views.SendNotificationAPIView.as_view(), name='send_notification'),
     path('league/global/status/', views.GlobalLeagueStatusView.as_view(), name='custom-user-league-status'), 
     path('league/company/status/', views.CompanyLeagueStatusView.as_view(), name='custom-user-company-league-status'),
-    path('api/company/dashboard/', views.CompanyDashboardView.as_view(), name='company-dashboard'),
+    path('company/dashboard/', views.CompanyDashboardView.as_view(), name='company-dashboard'),
      path('notifications/', views.NotificationsView.as_view(), name='notifications-list'),
 ]
