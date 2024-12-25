ea67e84b40c89414fa9ea2049339502e9816216e
Tsirimaholy <hei.tsirimaholy@gmail.com>
2024-12-25 12:04:39 +0300

    feat: support ticket consumer

--- myapp/routing.py
+++ myapp/routing.py
@@ -1,6 +1,7 @@
 # # chat/routing.py
 from django.urls import re_path, path
 from . import consumers
+from support import consumers as support_consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/test/', consumers.TestConsumer.as_asgi()),
@@ -11,10 +12,10 @@ websocket_urlpatterns = [
     path('ws/feed/user/<int:user_id>/', consumers.FeedConsumer.as_asgi()),
     path('ws/feed/company/', consumers.FeedConsumer.as_asgi()),
     path('ws/draw/<int:draw_id>/', consumers.DrawConsumer.as_asgi()),
-    path('ws/league/global/status/', consumers.CustomGlobalLeagueConsumer.as_asgi()), 
+    path('ws/league/global/status/', consumers.CustomGlobalLeagueConsumer.as_asgi()),
     path('ws/league/company/status/', consumers.CustomCompanyLeagueConsumer.as_asgi()),
     # path('ws/transaction/<int:user_id>/', consumers.TransactionConsumer.as_asgi()),
     path('ws/notification/', consumers.NotificationConsumer.as_asgi()),
+    re_path(r'ws/ticket/(?P<ticket_id>\w+)/$', support_consumers.TicketConsumer.as_asgi()),
     path('ws/<path:path>', consumers.InvalidPathConsumer.as_asgi()),
-
 ]
---
+++ support/consumers.py
@@ -0,0 +1,76 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Ticket, TicketMessage
+from .serializers import TicketMessageSerializer
+
+class TicketConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.ticket_id = self.scope['url_route']['kwargs']['ticket_id']
+        self.room_group_name = f'ticket_{self.ticket_id}'
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        # Check if user has access to this ticket
+        if not await self.can_access_ticket():
+            await self.close()
+            return
+
+        await self.accept()
+
+    async def disconnect(self, code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Save message to database
+        ticket_message = await self.save_message(message)
+
+        # Serialize the message
+        serialized_message = await self.serialize_message(ticket_message)
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': serialized_message
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps(message))
+
+    @database_sync_to_async
+    def can_access_ticket(self):
+        try:
+            ticket = Ticket.objects.get(id=self.ticket_id)
+            return ticket.company == self.scope['user'].company
+        except Ticket.DoesNotExist:
+            return False
+
+    @database_sync_to_async
+    def save_message(self, message):
+        ticket = Ticket.objects.get(id=self.ticket_id)
+        return TicketMessage.objects.create(
+            ticket=ticket,
+            sender=self.scope['user'],
+            message=message
+        )
+
+    @database_sync_to_async
+    def serialize_message(self, ticket_message):
+        return TicketMessageSerializer(ticket_message).data
