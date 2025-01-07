d0055a5fade6ea87c2be8c4d23c6db29381e9231
Tsirimaholy <hei.tsirimaholy@gmail.com>
2025-01-07 12:04:41 +0300

    feat(invitation): extract function for sending invitation

---
+++ myapp/user_service.py
@@ -0,0 +1,80 @@
+from myapp.models import CustomUser, Invitation
+from myapp.tasks import send_invitation_email_task
+import random
+import string
+
+
+def validate(email, inviter_user: CustomUser):
+    """
+    Validate the invitation email and inviter user.
+
+    Args:
+        email (str): The email of the user to invite.
+        inviter_user (CustomUser): The user sending the invitation.
+
+    Raises:
+        ValueError: If the inviter is inviting themselves or if the email belongs to an existing user who is a member of a company.
+    """
+    if email.lower() == inviter_user.email.lower():
+        raise ValueError({"error": "You cannot invite yourself."})
+    # Prevent inviting an existing user who already belongs to a company
+    if CustomUser.objects.filter(email=email, company__isnull=False).exists():
+        raise ValueError({"error": "The user is already a member of a company."})
+
+def send_invitation(email, first_name, last_name, inviter_user, inviter_company):
+    """
+      Sends an invitation to a user to join a company.
+
+      Args:
+          email (str): The email address of the invitee.
+          first_name (str): The first name of the invitee.
+          last_name (str): The last name of the invitee.
+          inviter_user (CustomUser): The user sending the invitation.
+          inviter_company (Company): The company the invitee is being invited to.
+
+      Returns:
+          Invitation: The created or existing invitation object.
+    """
+    user = inviter_user  # The user sending the invitation
+    company = inviter_company # Pass the company from the view
+
+    validate(email, user)
+
+    # Check if the user has already been invited to this company
+    existing_invitation = Invitation.objects.filter(email=email, company=company).first()
+    if existing_invitation:
+        # Send a reminder email asynchronously
+        send_invitation_email_task.delay_on_commit(
+            invite_code=existing_invitation.invite_code,
+            company_name=company.name,
+            inviter_name=user.username,
+            to_email=email
+        )
+        return existing_invitation
+
+    # Generate a 6-digit numeric code
+    invite_code = generate_digit_code()
+    # Send the invitation email asynchronously
+    send_invitation_email_task.delay_on_commit(
+        invite_code=invite_code,
+        company_name=company.name,
+        inviter_name=user.username,
+        to_email=email
+    )
+
+    # Create the invitation
+    invitation = Invitation.objects.create(
+        email=email,
+        first_name=first_name,
+        last_name=last_name,
+        company=company,
+        invite_code=invite_code,
+        invited_by=inviter_user
+    )
+
+    return invitation
+
+
+def generate_digit_code():
+    # Generate a 6-digit numeric code
+    return ''.join(random.choices(string.digits, k=6))
